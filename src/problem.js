0c8a330d14d07d0ae7f9a629de402b95cd0040db
Oleksandr <sorkin19822@ukr.net>
2023-05-25 19:26:43 +0300

    first

--- src/problem.js
+++ src/problem.js
@@ -1,13 +1,46 @@
+import {interval} from 'rxjs'
+import {filter} from 'rxjs/operators'
+
 const btn = document.getElementById('interval')
 const rxjsBtn = document.getElementById('rxjs')
 const display = document.querySelector('#problem .result')
 
 const people = [
-  {name: 'Vladilen', age: 25},
-  {name: 'Elena', age: 17},
-  {name: 'Ivan', age: 18},
-  {name: 'Igor', age: 14},
-  {name: 'Lisa', age: 32},
-  {name: 'Irina', age: 23},
-  {name: 'Oleg', age: 20}
+  {name: 'Vladilen', age: 25 },
+  {name: 'Elena', age: 17 },
+  {name: 'Ivan', age: 18 },
+  {name: 'Igor', age: 14 },
+  {name: 'Lisa', age: 32 },
+  {name: 'Irina', age: 23 },
+  {name: 'Oleg', age: 20 }
 ]
+
+btn.addEventListener('click', () => {
+  btn.disable = true
+  let i = 0
+  const canDrink = []
+
+  const interval = setInterval( () => {
+    if (people[i]) {
+      if (people[i].age >= 18) {
+        canDrink.push(people[i].name)
+      }
+      display.textContent = canDrink.join(' ')
+      i++
+    } else {
+      clearInterval(interval)
+      btn.disabled = false
+    }
+  }, 1000)
+})
+rxjsBtn.addEventListener('click', () => {
+  interval(1000)
+    .pipe(
+
+    )
+    .subscribe(
+      res => {
+        display.textContent = res
+      }
+    )
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path')
-const HTMLPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
